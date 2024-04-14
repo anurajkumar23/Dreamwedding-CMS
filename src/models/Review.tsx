8722e527d13c0d0493a40d0cf7564b6dda711269
Ishu Singh <104965112+ishuoncode@users.noreply.github.com>
2024-04-15 00:32:29 +0530

    model banquet

---
+++ src/models/Review.tsx
@@ -0,0 +1,27 @@
+import { Schema, model, models } from 'mongoose';
+
+const ReviewSchema = new Schema({
+    content: {
+        type: String,
+        required: [true, 'Review content cannot be blank']
+    },
+    rating: {
+        type: Number,
+        required: [true, 'Rating is required'],
+        min: 1,
+        max: 5
+    },
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'User ID is required']
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Review = models.Review || model("Review", ReviewSchema);
+
+export default Review;
