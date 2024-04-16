80676bc06cbd14d5bc04d207b11b80e7779d11ad
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2024-04-16 12:46:41 +0530

    connected to mongodb

---
+++ src/utils/database.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+let isConnected = false; // track the connection
+
+export const connectToDB = async () => {
+  mongoose.set('strictQuery', true);
+  if (isConnected) {
+    console.log('Mongo already connected');
+    return;
+  }
+  try {
+    const { connection} = await mongoose.connect(process.env.MONGODB_URI!, {
+      dbName: 'Dreamwedding',
+    });
+    isConnected = true;
+    console.log('MongoDB connected');
+    console.log(connection)
+  } catch (error) {
+    console.log("Not Connected")
+    console.log(error);
+  }
+};
