ad9c01fbbfaa7ec9c9b9ce8a7074d6806ad491a6
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2024-05-12 02:55:41 +0530

    small changes in file name

--- src/app/api/route.tsx
+++
@@ -1,9 +0,0 @@
-import Banquet from "@/models/banquet";
-import { connectToDB } from "@/utils/database";
-import { NextResponse } from "next/server";
-
-connectToDB();
-
-export async function GET(){
-   const banquet = await Banquet.find({})
-}
