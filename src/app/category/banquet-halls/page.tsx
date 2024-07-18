d8af89c60c6742a31aa905e79f479ec2b20ade21
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2024-07-18 11:40:14 +0530

    add all page components

---
+++ src/app/category/banquet-halls/page.tsx
@@ -0,0 +1,13 @@
+import { Separator } from '@/components/ui/separator'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="px-8 py-10">
+    <p className="text-heading2-bold text-white">BanquetHall Data</p>
+    <Separator className="bg-white my-5" />
+    </div>
+  )
+}
+
+export default page
