4ee1565b1df4266646ee2fa467ee1e4a0e3284c2
Ishu Singh <104965112+ishuoncode@users.noreply.github.com>
2024-09-16 01:39:44 +0530

    add bassic filter

--- src/app/api/banquet/route.ts
+++ src/app/api/banquet/route.ts
@@ -7,13 +7,49 @@ import { NextRequest, NextResponse } from "next/server";
 
 connectToDB()
 
-export async function GET(){
- 
-    const banquet = await handleGet(Banquet, "banquet");
+export async function GET(req: Request) {
+    try {
+      const { searchParams } = new URL(req.url);
+      console.log(searchParams,"paramas")
 
-    return banquet;
-}
+      const filters: any = {};
 
+      for (const [key, value] of searchParams) {
+        switch (key) {
+          case 'rating':
+            if (value === 'true') {
+              filters.rating = { $gte: 4 };
+            }
+            break;
+          case 'budgetFriendly':
+            if (value === 'true') {
+              filters.price = { $lte: 30000 }; 
+            }
+            break;
+         
+        }
+      }
+  
+
+      console.log(filters,"fimters")
+
+      const banquet = await Banquet.find(filters);
+      
+      // If banquet data is successfully fetched, return success response
+      return NextResponse.json({
+        message: "success",
+        length: banquet.length,
+        data: banquet,
+      });
+    } catch (error: any) {
+      // Log and return error response if something goes wrong
+      console.error("Error fetching banquet data:", error);
+      return NextResponse.json(
+        { message: "error", error: error.message },
+        { status: 500 }
+      );
+    }
+  }
 export async function POST(req: NextRequest, res: NextResponse) {
     
     const updatedData = await banquetMiddleware(req)
